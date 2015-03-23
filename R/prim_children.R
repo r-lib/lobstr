1955fa2e276af15d9a61db827837d8dc58348069
hadley <h.wickham@gmail.com>
2015-03-23 14:23:18 -0500

    Primitive children

--- R/prim_children.R
+++ R/prim_children.R
@@ -1,3 +1,13 @@
+#' Primitive children.
+#'
+#' Extract all the low-level children of an element. This works at a low-level:
+#' all dispatch is done in C on the SEXPTYPE. No S3 dispatch is performed.
+#'
+#' @param x An object.
+#' @export
+#' @return A list of children with class "primlist". This list should be
+#'   considered read-only. DO NOT modify any elements returned by this
+#'   function.
 prim_children <- function(x) {
   structure(prim_children_(x), class = "primlist")
 }
