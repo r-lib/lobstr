36aa10cf4f2be449d3c7aaecb806f7e3532709a1
hadley <h.wickham@gmail.com>
2018-03-06 08:12:34 -0600

    obj_size improvements
    * rename from object_size
    * namespaced S3 class name
    * reduce object.size positioning
    * remove compare_size
    * add c method

--- R/prim_size.R
+++ R/prim_size.R
@@ -1,68 +1,64 @@
 #' Calculate the size of an object.
 #'
-#' \code{object_size} works similarly to \code{\link{object.size}}, but counts
-#' more accurately and includes the size of environments. \code{compare_size}
-#' makes it easy to compare the output of \code{object_size} and
-#' \code{object.size}.
+#' \code{obj_size} works similarly to \code{\link{object.size}}, but correctly
+#' takes into account shared values, and includes the size of environments.
 #'
 #' @section Environments:
-#'
-#' \code{object_size} attempts to take into account the size of the
+#' \code{obj_size} attempts to take into account the size of the
 #' environments associated with an object. This is particularly important
 #' for closures and formulas, since otherwise you may not realise that you've
 #' accidentally captured a large object. However, it's easy to over count:
 #' you don't want to include the size of every object in every environment
-#' leading back to the \code{\link{emptyenv}()}. \code{object_size} takes
+#' leading back to the \code{\link{emptyenv}()}. \code{obj_size} takes
 #' a heuristic approach: it never counts the size of the global env,
 #' the base env, the empty env, or any namespace.
 #'
 #' Additionally, the \code{env} argument allows you to specify another
 #' environment at which to stop. This defaults to the environment from which
-#' \code{object_size} is called to prevent double-counting of objects created
+#' \code{obj_size} is called to prevent double-counting of objects created
 #' elsewhere.
 #'
 #' @export
-#' @examples
-#' # object.size doesn't keep track of shared elements in an object
-#' # object_size does
-#' x <- 1:1e4
-#' z <- list(x, x, x)
-#' object.size(z)
-#' object_size(z)
-#'
-#' # Or in one function:
-#' compare_size(z)
-#'
-#' # this means that object_size is not transitive
-#' object_size(x)
-#' object_size(z)
-#' object_size(x, z)
-#'
-#' # object.size doesn't include the size of environments, which makes
-#' # it easy to miss objects that are carrying around large environments
-#' f <- function() {
-#'   x <- 1:1e4
-#'   a ~ b
-#' }
-#' compare_size(f())
-#' @param x,... Set of objects to compute total size.
+#' @param ... Set of objects to compute size.
 #' @param env Environment in which to terminate search. This defaults to the
 #'   current environment so that you don't include the size of objects that
 #'   are already stored elsewhere.
 #' @return An estimate of the size of the object, in bytes.
-object_size <- function(..., env = parent.frame()) {
+#' @examples
+#' # obj_size correctly accounts for shared references
+#' x <- 1:1e4
+#' obj_size(x)
+#'
+#' z <- list(x, x, x)
+#' obj_size(z)
+#'
+#' # this means that object size is not transitive
+#' obj_size(x)
+#' obj_size(z)
+#' obj_size(x, z)
+#'
+#' # obj_size() also includes the size of environments
+#' f <- function() {
+#'   x <- 1:1e4
+#'   a ~ b
+#' }
+#' obj_size(f())
+obj_size <- function(..., env = parent.frame()) {
   size <- prim_sizes(list(...), env)
-  structure(size, class = "bytes")
+  new_bytes(size)
+}
+
+new_bytes <- function(x) {
+  structure(x, class = "lobstr_bytes")
 }
 
 #' @export
-#' @rdname object_size
-compare_size <- function(x) {
-  c(base = utils::object.size(x), lobstr = object_size(x))
-}
-
-#' @export
-print.bytes <- function(x, digits = 3, ...) {
+print.lobstr_bytes <- function(x, digits = 3, ...) {
   fx <- format(x, big.mark = ",")
-  cat(fx, " B\n", sep = "")
+  cat(paste0(fx, " B", "\n", collapse = ""))
+}
+
+#' @export
+c.lobstr_bytes <- function(...) {
+  new_bytes(NextMethod())
 }
