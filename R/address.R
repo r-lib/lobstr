ffc49ffe2e3f8f1c24a7f00414e0d84015080078
hadley <h.wickham@gmail.com>
2018-03-07 07:43:30 -0600

    Rename obj_address to obj_addr

--- R/address.R
+++ R/address.R
@@ -1,7 +1,7 @@
 #' Find memory location of objects and their children.
 #'
-#' `obj_address()` gives the address of the value that `x` points to;
-#' `obj_addresses()` gives the address of the components the list,
+#' `obj_addr()` gives the address of the value that `x` points to;
+#' `obj_addrs()` gives the address of the components the list,
 #' environment, and character vector `x` point to
 #'
 #' @param x An object
@@ -10,32 +10,32 @@
 #' # R creates copies lazily
 #' x <- 1:10
 #' y <- x
-#' obj_address(x) == obj_address(y)
+#' obj_addr(x) == obj_addr(y)
 #'
 #' y[1] <- 2L
-#' obj_address(x) == obj_address(y)
+#' obj_addr(x) == obj_addr(y)
 #'
 #' y <- runif(10)
-#' obj_address(y)
+#' obj_addr(y)
 #' z <- list(y, y)
-#' obj_addresses(z)
+#' obj_addrs(z)
 #'
 #' y[2] <- 1.0
-#' obj_addresses(z)
-#' obj_address(y)
+#' obj_addrs(z)
+#' obj_addr(y)
 #'
 #' # The address of an object is different every time you create it:
-#' obj_address(1:10)
-#' obj_address(1:10)
-#' obj_address(1:10)
-obj_address <- function(x) {
+#' obj_addr(1:10)
+#' obj_addr(1:10)
+#' obj_addr(1:10)
+obj_addr <- function(x) {
   x <- enquo(x)
   prim_address_(quo_get_expr(x), quo_get_env(x))
 }
 
 #' @export
-#' @rdname obj_address
-obj_addresses <- function(x) {
+#' @rdname obj_addr
+obj_addrs <- function(x) {
   x <- enquo(x)
   prim_addresses_(quo_get_expr(x), quo_get_env(x))
 }
--- tests/testthat/test-address.R
+++ tests/testthat/test-address.R
@@ -1,23 +1,23 @@
 context("test-address.R")
 
 test_that("address of expression varies", {
-  a <- obj_address(1:10)
-  b <- obj_address(1:10)
+  a <- obj_addr(1:10)
+  b <- obj_addr(1:10)
   expect_false(identical(a, b))
 })
 
 test_that("address of variable is constant", {
   x <- 1:10
-  expect_equal(obj_address(x), obj_address(x))
+  expect_equal(obj_addr(x), obj_addr(x))
 })
 
 test_that("address flows through function wrappers", {
   x <- 1:10
-  f <- function(x) obj_address(x)
+  f <- function(x) obj_addr(x)
   g <- function(y) f(y)
   h <- function(z) g(z)
 
-  address <- obj_address(x)
+  address <- obj_addr(x)
   expect_equal(f(x), address)
   expect_equal(g(x), address)
   expect_equal(h(x), address)
@@ -29,33 +29,33 @@ test_that("address flows through function wrappers", {
 test_that("can find addresses of list elements", {
   x <- 1:3
   y <- 1:3
-  addr <- c(obj_address(x), obj_address(y))
+  addr <- c(obj_addr(x), obj_addr(y))
 
   l <- list(x, y)
-  expect_equal(obj_addresses(l), addr)
+  expect_equal(obj_addrs(l), addr)
 })
 
 test_that("can find addresses of environment elements", {
   x <- 1:3
   y <- 1:3
-  addr <- c(obj_address(x), obj_address(y))
+  addr <- c(obj_addr(x), obj_addr(y))
 
   e1 <- new.env(hash = TRUE)
   e1$x <- x
   e1$y <- y
-  expect_setequal(obj_addresses(e1), addr)
+  expect_setequal(obj_addrs(e1), addr)
 
   e2 <- new.env(hash = FALSE)
   e2$x <- x
   e2$y <- y
-  expect_setequal(obj_addresses(e2), addr)
+  expect_setequal(obj_addrs(e2), addr)
 })
 
 test_that("address of character vectors points to global string pool", {
-  addr <- obj_addresses(c("a", "a", "a"))
+  addr <- obj_addrs(c("a", "a", "a"))
   expect_equal(addr[[1]], addr[[2]])
 })
 
 test_that("addresses of other elements throws errors", {
-  expect_error(obj_addresses(1:10), "must be a list")
+  expect_error(obj_addrs(1:10), "must be a list")
 })
