b4e3d2001683e778732859431cff5b79eaf51f64
Nick Strayer <nick.strayer@gmail.com>
2021-04-06 11:04:48 -0400

    Converted most tests to snapshot tests

---
+++ tests/testthat/_snaps/tree.md
@@ -0,0 +1,243 @@
+# Array-like indices can be shown or hidden
+
+    Code
+      nested_lists <- list(list(id = "a", val = 2), list(id = "b", val = 1, children = list(
+        list(id = "b1", val = 2.5), list(id = "b2", val = 8, children = list(list(id = "b21",
+          val = 4))))), list(id = "c", val = 8, children = list(list(id = "c1"), list(
+        id = "c2", val = 1))))
+      tree(nested_lists, index_arraylike = TRUE)
+    Output
+      [list]
+      ├─1:{list}
+      │ ├─id:"a"
+      │ └─val:2
+      ├─2:{list}
+      │ ├─id:"b"
+      │ ├─val:1
+      │ └─children:[list]
+      │   ├─1:{list}
+      │   │ ├─id:"b1"
+      │   │ └─val:2.5
+      │   └─2:{list}
+      │     ├─id:"b2"
+      │     ├─val:8
+      │     └─children:[list]
+      │       └─1:{list}
+      │         ├─id:"b21"
+      │         └─val:4
+      └─3:{list}
+        ├─id:"c"
+        ├─val:8
+        └─children:[list]
+          ├─1:{list}
+          │ └─id:"c1"
+          └─2:{list}
+            ├─id:"c2"
+            └─val:1
+    Code
+      tree(nested_lists, index_arraylike = FALSE)
+    Output
+      [list]
+      ├─{list}
+      │ ├─id:"a"
+      │ └─val:2
+      ├─{list}
+      │ ├─id:"b"
+      │ ├─val:1
+      │ └─children:[list]
+      │   ├─{list}
+      │   │ ├─id:"b1"
+      │   │ └─val:2.5
+      │   └─{list}
+      │     ├─id:"b2"
+      │     ├─val:8
+      │     └─children:[list]
+      │       └─{list}
+      │         ├─id:"b21"
+      │         └─val:4
+      └─{list}
+        ├─id:"c"
+        ├─val:8
+        └─children:[list]
+          ├─{list}
+          │ └─id:"c1"
+          └─{list}
+            ├─id:"c2"
+            └─val:1
+
+# Atomic arrays have sensible defaults with truncation added for longer than 10-elements
+
+    Code
+      tree(list(name = "vectored list", num_vec = 1:10, char_vec = letters))
+    Output
+      {list}
+      ├─name:"vectored list"
+      ├─num_vec:1,2,3,4,5,6,7,8,9,10
+      └─char_vec:"a","b","c","d","e","f","g","h","i","j",...(n = 26)
+
+# Works with HTML tag structures
+
+    Code
+      tree(shiny::sliderInput("test", "Input Label", 0, 1, 0.5))
+    Output
+      {shiny.tag}
+      ├─name:"div"
+      ├─attribs:{list}
+      │ └─class:"form-group shiny-input-container"
+      └─children:[list]
+        ├─1:{shiny.tag}
+        │ ├─name:"label"
+        │ ├─attribs:{list}
+        │ │ ├─class:"control-label"
+        │ │ ├─id:"test-label"
+        │ │ └─for:"test"
+        │ └─children:[list]
+        │   └─1:"Input Label"
+        └─2:{shiny.tag}
+          ├─name:"input"
+          ├─attribs:{list}
+          │ ├─class:"js-range-slider"
+          │ ├─id:"test"
+          │ ├─data-skin:"shiny"
+          │ ├─data-min:"0"
+          │ ├─data-max:"1"
+          │ ├─data-from:"0.5"
+          │ ├─data-step:"0.01"
+          │ ├─data-grid:"true"
+          │ ├─data-grid-num:10
+          │ ├─data-grid-snap:"false"
+          │ ├─data-prettify-separator:","
+          │ ├─data-prettify-enabled:"true"
+          │ ├─data-keyboard:"true"
+          │ └─data-data-type:"number"
+          └─children:[list]
+
+# Max depth can be enforced
+
+    Code
+      deep_list <- list(list(id = "a", val = 2), list(id = "b", val = 1, children = list(
+        list(id = "b1", val = 2.5), list(id = "b2", val = 8, children = list(list(id = "b21",
+          val = 4))))))
+      tree(deep_list, max_depth = 1)
+    Output
+      [list]
+      ├─1:{list}
+      │ ├─id:"a"
+      │ └─val:2
+      └─2:{list}
+        ├─id:"b"
+        ├─val:1
+        └─children:[list]
+    Code
+      tree(deep_list, max_depth = 2)
+    Output
+      [list]
+      ├─1:{list}
+      │ ├─id:"a"
+      │ └─val:2
+      └─2:{list}
+        ├─id:"b"
+        ├─val:1
+        └─children:[list]
+          ├─1:{list}
+          └─2:{list}
+    Code
+      tree(deep_list, max_depth = 3)
+    Output
+      [list]
+      ├─1:{list}
+      │ ├─id:"a"
+      │ └─val:2
+      └─2:{list}
+        ├─id:"b"
+        ├─val:1
+        └─children:[list]
+          ├─1:{list}
+          │ ├─id:"b1"
+          │ └─val:2.5
+          └─2:{list}
+            ├─id:"b2"
+            ├─val:8
+            └─children:[list]
+
+# Attributes are properly displayed as special children nodes
+
+    Code
+      list_w_attrs <- structure(list(structure(list(id = "a", val = 2), level = 2,
+      name = "first child"), structure(list(id = "b", val = 1, children = list(list(
+        id = "b1", val = 2.5))), level = 2, name = "second child", class = "custom-class"),
+      level = "1", name = "root"))
+      tree(list_w_attrs, show_attributes = TRUE)
+    Output
+      {list}
+      ├─{list}
+      │ ├─id:"a"
+      │ ├─val:2
+      │ ├┄<attr>names:"id","val"
+      │ ├┄<attr>level:2
+      │ └┄<attr>name:"first child"
+      ├─{custom-class}
+      │ ├─id:"b"
+      │ ├─val:1
+      │ ├─children:[list]
+      │ ┊ └─1:{list}
+      │ ┊   ├─id:"b1"
+      │ ┊   ├─val:2.5
+      │ ┊   └┄<attr>names:"id","val"
+      │ ├┄<attr>names:"id","val","children"
+      │ ├┄<attr>level:2
+      │ ├┄<attr>name:"second child"
+      │ └┄<attr>class:"custom-class"
+      ├─level:"1"
+      ├─name:"root"
+      └┄<attr>names:"","","level","name"
+    Code
+      tree(list_w_attrs, show_attributes = FALSE)
+    Output
+      {list}
+      ├─{list}
+      │ ├─id:"a"
+      │ └─val:2
+      ├─{custom-class}
+      │ ├─id:"b"
+      │ ├─val:1
+      │ └─children:[list]
+      │   └─1:{list}
+      │     ├─id:"b1"
+      │     └─val:2.5
+      ├─level:"1"
+      └─name:"root"
+
+# Handles elements with a single element and attributes well
+
+    Code
+      tree(list("first element", structure("second element", purpose = "show bug")),
+      show_attributes = TRUE)
+    Output
+      [list]
+      ├─1:"first element"
+      └─2:"second element"
+        └┄<attr>purpose:"show bug"
+
+# Large and multiline strings are handled gracefully
+
+    Code
+      long_strings <- list(`normal string` = "first element", `really long string` = paste(
+        rep(letters, 4), collapse = ""), `vec of long strings` = c(
+        "a long\nand multi\nline string element", "a fine length",
+        "another long\nand also multi\nline string element"))
+      tree(long_strings)
+    Output
+      {list}
+      ├─normal string:"first element"
+      ├─really long string:"abcdefghijklmnopqrstuvwxyzabcdef..."
+      └─vec of long strings:"a long and m...","a fine length","another long..."
+    Code
+      tree(long_strings, remove_newlines = FALSE)
+    Output
+      {list}
+      ├─normal string:"first element"
+      ├─really long string:"abcdefghijklmnopqrstuvwxyzabcdef..."
+      └─vec of long strings:"a long
+      and m...","a fine length","another long..."
+
--- tests/testthat/test-tree.R
+++ tests/testthat/test-tree.R
@@ -1,3 +1,64 @@
+
+
+test_that("Array-like indices can be shown or hidden", {
+  expect_snapshot({
+    nested_lists <- list(
+      list(id = "a", val = 2),
+      list(
+        id = "b",
+        val = 1,
+        children = list(
+          list(id = "b1",val = 2.5),
+          list(
+            id = "b2",
+            val = 8,
+            children = list(
+              list(id = "b21", val = 4)
+            )
+          )
+        )
+      ),
+      list(
+        id = "c",
+        val = 8,
+        children = list(
+          list(id = "c1"),
+          list(id = "c2", val = 1)
+        )
+      )
+    )
+
+    tree(nested_lists, index_arraylike = TRUE)
+
+    tree(nested_lists, index_arraylike = FALSE)
+  })
+
+})
+
+
+test_that(
+  "Atomic arrays have sensible defaults with truncation added for longer than 10-elements",
+  {
+    expect_snapshot(
+      tree(
+        list(
+          name = "vectored list",
+          num_vec = 1:10,
+          char_vec = letters
+        )
+      )
+    )
+
+  })
+
+test_that("Works with HTML tag structures", {
+  # sliderInput is a pretty complex structure all in one line
+  expect_snapshot(
+    tree(shiny::sliderInput("test", "Input Label", 0,1,0.5))
+  )
+})
+
+
 # Builds a regex that tests for the _lack_ of a substring
 doesNotExistRegex <- function(substring){
   # Uses a negative look-behind to check for the absence of the substring
@@ -9,94 +70,33 @@ doesNotExistRegex <- function(substring){
   paste0("(?sm)^(?!.*", substring, ").*$")
 }
 
-
-# These tests are disabled because they require snapshot testing which is not available until testthat 3
-# test_that("Pure nested lists work", {
-#   plain_list <- list(
-#     list(id = "a",
-#          val = 2),
-#     list(id = "b",
-#          val = 1,
-#          children = list(
-#            list(id = "b1",
-#                 val = 2.5),
-#            list(id = "b2",
-#                 val = 8,
-#                 children = list(
-#                   list(id = "b21",
-#                        val = 4)
-#                 )))),
-#     list(id = "c",
-#          val = 8,
-#          children = list(
-#            list(),
-#            list(id = "c1"),
-#            list(id = "c2",
-#                 val = 1))))
-#
-#   expect_snapshot(tree(plain_list))
-#
-#   # Also with array-likes without indices
-#   expect_snapshot(tree(plain_list, index_arraylike = FALSE))
-# })
-#
-#
-# test_that("Atomic arrays have sensible defaults with truncation added for longer than 10-elements",{
-#   list_w_vectors <- list(
-#     name = "vectored list",
-#     num_vec = 1:10,
-#     char_vec = letters
-#   )
-#   expect_snapshot(
-#     tree(list_w_vectors)
-#   )
-# })
-#
-# test_that("Works with HTML tag structures", {
-#   # sliderInput is a pretty complex structure all in one line
-#   expect_snapshot(
-#     tree(shiny::sliderInput("test", "Input Label", 0,1,0.5))
-#   )
-# })
-
 test_that("Max depth can be enforced", {
-  plain_list <- list(
-    list(id = "a",
-         val = 2),
-    list(id = "b",
-         val = 1,
-         children = list(
-           list(id = "b1",
-                val = 2.5),
-           list(id = "b2",
-                val = 8,
-                children = list(
-                  list(id = "b21",
-                       val = 4)
-                )))),
-    list(id = "c",
-         val = 8,
-         children = list(
-           list(),
-           list(id = "c1"),
-           list(id = "c2",
-                val = 1))))
 
-  expect_output(
-    tree(plain_list, max_depth = 2),
-    regexp = doesNotExistRegex("b21"),
-    perl = TRUE
-  )
-
-  expect_output(
-    tree(plain_list),
-    regexp = "b21",
-    fixed = TRUE
-  )
+  expect_snapshot({
+    deep_list <- list(
+      list(id = "a", val = 2),
+      list(
+        id = "b",
+        val = 1,
+        children = list(
+          list(id = "b1",val = 2.5),
+          list(
+            id = "b2",
+            val = 8,
+            children = list(
+              list(id = "b21", val = 4)
+            )
+          )
+        )
+      )
+    )
+    tree(deep_list, max_depth = 1)
+    tree(deep_list, max_depth = 2)
+    tree(deep_list, max_depth = 3)
+  })
 })
 
-test_that("Null printing", {
-
+test_that("Null values are caught and printed properly", {
   expect_output(
     tree(list("null-element" = NULL)),
     regexp = "null-element:<NULL>",
@@ -105,7 +105,6 @@ test_that("Null printing", {
 })
 
 test_that("NA printing", {
-
   expect_output(
     tree(list("NA-element" = NA)),
     regexp = "NA-element:NA",
@@ -115,129 +114,90 @@ test_that("NA printing", {
 
 test_that("non-named elements in named list",{
   expect_output(
-    tree(list(
-      "a" = 1,
-      "element without id"
-    ),
-    char_horizontal = "\u2500"),
+    tree(
+      list(
+        "a" = 1,
+        "element without id"
+      ),
+      char_horizontal = "\u2500"),
     "\u2500\"element without id\"",
     fixed = TRUE
   )
 })
 
 
-test_that("Attributes are properly displayed as a special child node", {
-  list_w_attrs <- structure(
-    list(
-      structure(
-        list(id = "a",
-             val = 2),
-        level = 2,
-        name = "first child"
-      ),
-      structure(
-        list(id = "b",
-             val = 1,
-             children = list(
-               list(id = "b1",
-                    val = 2.5),
-               list(id = "b2",
-                    val = 8,
-                    children = list(
-                      list(id = "b21",
-                           val = 4)
-                    )))),
-        level = 2,
-        name = "second child",
-        class = "custom-class"
-      )),
-    level = "1",
-    name = "root"
-  )
+test_that("Attributes are properly displayed as special children nodes", {
 
-  # Disabled until testthat 3
-  # expect_snapshot(tree(list_w_attrs, show_attributes = TRUE))
+  expect_snapshot({
+    list_w_attrs <- structure(
+      list(
+        structure(
+          list(id = "a", val = 2),
+          level = 2,
+          name = "first child"
+        ),
+        structure(
+          list(
+            id = "b",
+            val = 1,
+            children = list(
+              list(id = "b1", val = 2.5)
+            )
+          ),
+          level = 2,
+          name = "second child",
+          class = "custom-class"
+        ),
+        level = "1",
+        name = "root"
+      )
+    )
 
-  expect_output(
-    tree(list_w_attrs, show_attributes = FALSE),
-    # Should _not_ contain text attr anywhere
-    # The (?...) block controls how the perl regex parser works
-    # s -> Makes a dot matches all characters
-    # m -> Multiline mode
-    regexp = doesNotExistRegex("attr"),
-    perl = TRUE
-  )
+    # Shows attributes
+    tree(list_w_attrs, show_attributes = TRUE)
 
-  expect_output(
-    tree(list_w_attrs, show_attributes = TRUE),
-    regexp = "attr",
-    fixed = TRUE
-  )
+    # Hides attributes (default)
+    tree(list_w_attrs, show_attributes = FALSE)
+  })
 })
 
 test_that("Handles elements with a single element and attributes well", {
   # The logic of handling single-element nodes with attributes is tricky
   # This test _should_ catch mistakes
-  simple_string <- list(
-    "first element",
-    structure(
-      "second element",
-      purpose = "show bug"
-    ))
+  expect_snapshot({
+    tree(
+      list(
+        "first element",
+        structure(
+          "second element",
+          purpose = "show bug"
+        )
+      ),
+      show_attributes = TRUE
+    )
+  })
 
-  # Hard-code the last child symbol in-case the default changes
-  last_child_symbol <- "\u2570"
-
-  expect_output(
-    tree(simple_string, show_attributes = TRUE, char_final_branch = last_child_symbol),
-    regexp = doesNotExistRegex( paste0(last_child_symbol, ".*", last_child_symbol)),
-    perl = TRUE
-  )
 })
 
 test_that("Large and multiline strings are handled gracefully", {
-  long_strings <- list(
-    "normal string" = "first element",
-    "really long string" = paste(rep(letters, 4), collapse = ""),
-    "vec of long strings" = c(
-      "a long\nand multi\nline string element",
-      "a fine length",
-      "another long\nand also multi\nline string element"
-    ))
+  expect_snapshot({
+    long_strings <- list(
+      "normal string" = "first element",
+      "really long string" = paste(rep(letters, 4), collapse = ""),
+      "vec of long strings" = c(
+        "a long\nand multi\nline string element",
+        "a fine length",
+        "another long\nand also multi\nline string element"
+      )
+    )
 
-  # No truncation of first string
-  expect_output(
-    tree(long_strings),
-    regexp = 'normal string:"first element"',
-    fixed = TRUE
-  )
+    # No truncation of first string
+    # Really long single string is truncated and elipsesed
+    # Short string inside vector with long strings is not truncated
+    tree(long_strings)
 
-  # Really long single string is truncated and elipsesed
-  expect_output(
-    tree(long_strings),
-    regexp = 'really long string:"abcdefghijklmnopqrstuvwxyzabcdef..."',
-    fixed = TRUE
-  )
-
-  # Short string inside vector with long strings is not truncated
-  expect_output(
-    tree(long_strings),
-    regexp = ',"a fine length",',
-    fixed = TRUE
-  )
-
-  # But it's long-siblings are
-  expect_output(
-    tree(long_strings),
-    regexp = '"another long and also..."',
-    fixed = TRUE
-  )
-
-  # Newline removal can be disabled
-  expect_output(
-    tree(long_strings, remove_newlines = FALSE),
-    regexp = '"a long\n',
-    fixed = TRUE
-  )
+    # Newline removal can be disabled
+    tree(long_strings, remove_newlines = FALSE)
+  })
 })
 
